3563f806b67b74e6eac826703dfdcf2887413345
Saketh <saketh.pulipaka@trinet.com>
2016-09-20 16:34:05 -0700

    The Hero Editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    //1-way data Binding
+    <input value ="{{hero.name}}" placeholder="name">
+    // 2-way Data Binding 
+    //<input [(ngModel)]="hero.name" placeholder="name">
+
+  </div>`
+
 })
-export class AppComponent { }
+export class AppComponent { 
+    title =  'My First Angular App';
+    hero : Hero = {
+        id : 1,
+        name : 'Windstorm',
+
+    }
+}
+
+export class Hero {
+    id : number;
+    name : string;
+
+}
+
